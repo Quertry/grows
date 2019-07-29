b7dbc5f5967960cd019f826e929df7a49b2fbb66
Yo <t4vo.98@gmail.com>
2019-07-29 01:33:28 -0500

    04

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<path:path>')
-def Index():
+async def Index(request, path=""):
     return render_template('index.html')
 
 if __name__ == "__main__":
